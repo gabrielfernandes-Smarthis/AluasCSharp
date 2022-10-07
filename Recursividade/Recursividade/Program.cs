8c72cb63667f781740b1c27373e3fa289308471b
Gabriel Gomes <ggfernandes30@gmail.com>
2022-10-06 23:31:23 -0300

    Recursividade em C#

---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursiva m = new Recursiva();
+            m.ExecutarRecursivo("Olá", 5);
+        }
+    }
+}
