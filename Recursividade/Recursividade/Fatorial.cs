14b23efa7334dd83594d1712582ce62f03b3efeb
Gabriel Gomes <ggfernandes30@gmail.com>
2022-10-19 12:03:39 -0300

    Estudo recursividade

---
+++ Recursividade/Recursividade/Fatorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    public class Fatorial
+    {
+        public int CalculoFatorial(int num)
+        {
+            if (num <= 0)
+            {
+                return 1;
+            }
+            return num * CalculoFatorial(num - 1);
+        }
+    }
+}
--- Recursividade/Recursividade/Program.cs
+++ Recursividade/Recursividade/Program.cs
@@ -10,8 +10,18 @@ namespace Recursividade
     {
         static void Main(string[] args)
         {
-            Recursiva m = new Recursiva();
-            m.ExecutarRecursivo("Olá", 5);
+            //RECURSIVIDADEf
+            //Recursiva m = new Recursiva();
+            //m.ExecutarRecursivo("Olá", 5);
+
+            //Exercicio Recursividade com fatorial
+            Fatorial f = new Fatorial();
+            Console.Write("Escolha um numero para calcular o fatorial: ");
+            int num = int.Parse(Console.ReadLine());
+            int fatorial = f.CalculoFatorial(num);
+            Console.WriteLine("Resultado do calculo do fatorial de {0}: {1}", num, fatorial);
+
+            Console.ReadKey();
         }
     }
 }
