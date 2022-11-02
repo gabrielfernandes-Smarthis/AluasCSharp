938dc4d237174beceea0b9ee350be0ece59a0c1c
Gabriel Gomes <ggfernandes30@gmail.com>
2022-11-02 11:57:03 -0300

    Estudo de colecoes importantes em C#

---
+++ Colecoes/Colecoes/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Colecoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnHashSet = new System.Windows.Forms.Button();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 25;
+            this.lista.Location = new System.Drawing.Point(12, 12);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(734, 479);
+            this.lista.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(752, 446);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(185, 45);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnHashSet
+            // 
+            this.btnHashSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHashSet.Location = new System.Drawing.Point(752, 395);
+            this.btnHashSet.Name = "btnHashSet";
+            this.btnHashSet.Size = new System.Drawing.Size(185, 45);
+            this.btnHashSet.TabIndex = 2;
+            this.btnHashSet.Text = "HashSet";
+            this.btnHashSet.UseVisualStyleBackColor = true;
+            this.btnHashSet.Click += new System.EventHandler(this.btnHashSet_Click);
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDictionary.Location = new System.Drawing.Point(752, 344);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(185, 45);
+            this.btnDictionary.TabIndex = 3;
+            this.btnDictionary.Text = "Dictionary";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 512);
+            this.Controls.Add(this.btnDictionary);
+            this.Controls.Add(this.btnHashSet);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Lista Generica";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnHashSet;
+        private System.Windows.Forms.Button btnDictionary;
+    }
+}
+
---
+++ Colecoes/Colecoes/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            //Array
+            string[] nomesArray = new string[3];
+            nomesArray[0] = "Pedro";
+            nomesArray[1] = "Leandro";
+            nomesArray[2] = "Eduarda";
+
+            //Lista
+            List<string> nomes = new List<string>();
+            nomes.Add("Gabriel");
+            nomes.Add("Marina");
+            nomes.AddRange(nomesArray);
+
+            if (nomes.Remove("Leandro"))
+            {
+                MessageBox.Show("Lucas Removido");
+            }
+            else
+            {
+                MessageBox.Show("Nao foi possivel remover");
+            }
+
+            if (nomes.Contains("Cleiton"))
+            {
+                MessageBox.Show("Contem");
+            }
+            else
+            {
+                MessageBox.Show("Nao contem");
+            }
+
+            MessageBox.Show("Numero de elementos na lista: " + nomes.Count);
+
+            //nomes.Sort();
+
+            MessageBox.Show("Gabriel esta no indice: " + nomes.IndexOf("Gabriel"));
+
+            nomes.Insert(2, "Cleiton");
+            nomes.RemoveAt(3);
+
+            foreach (string n in nomes)
+            {
+                lista.Items.Add(n);
+            }
+        }
+
+        private void btnHashSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            HashSet<string> veiculos = new HashSet<string>()
+            {
+                "Carro",
+                "Moto",
+                "Aviao",
+                "Navio",
+                "Carro"
+            };
+
+            if (veiculos.Add("Carro"))
+            {
+                MessageBox.Show("Carro adicionado");
+            }
+            else
+            {
+                MessageBox.Show("Carro nao adicionado");
+            }
+
+            if (veiculos.Contains("Navio"))
+            {
+                MessageBox.Show("Contem");
+            }
+            else
+            {
+                MessageBox.Show("Nao Contem");
+            }
+
+            veiculos.Remove("Carro");
+            MessageBox.Show("Numero de elementos no HashSet: " + veiculos.Count);
+            MessageBox.Show(veiculos.ElementAt(0));
+
+            foreach (string v in veiculos)
+            {
+                lista.Items.Add(v);
+            }
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> alunos = new Dictionary<int, string>()
+            {
+                { 2210550 , "Lucas" },
+                { 2210551 , "Pedro" },
+                { 2210552 , "Leandro" },
+                { 2210553 , "Eduarda" }
+            };
+
+            alunos.Add(2210480, "Gabriel Gomes");
+            
+            if (alunos.ContainsKey(2210550))
+            {
+                MessageBox.Show("O aluno existe");
+            }
+            else
+            {
+                MessageBox.Show("O aluno nao existe");
+            }
+            
+            if (alunos.ContainsValue("Lucas"))
+            {
+                MessageBox.Show("O aluno existe");
+            }
+            else
+            {
+                MessageBox.Show("O aluno nao existe");
+            }
+            
+            foreach (KeyValuePair<int, string> a in alunos)
+            {
+                lista.Items.Add(a.Value);
+            }
+        }
+    }
+}
