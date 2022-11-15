1ca32ebea8cdf2ead653365c9e322a338138e604
Gabriel Gomes <ggfernandes30@gmail.com>
2022-11-15 17:53:06 -0300

    Criacao de sockets para o cliente e para o servidor se comunicarem

---
+++ SocketCliente/SocketCliente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketCliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                socket.Connect(endPoint);
+                Console.WriteLine("Conectado com exito");
+                Console.WriteLine("Insira uma informacao para enviar: \n");
+
+                string info = Console.ReadLine();
+                byte[] infoEnviar = Encoding.UTF8.GetBytes(info);
+
+                socket.Send(infoEnviar, 0, infoEnviar.Length, SocketFlags.None);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Nao foi possivel conectar ao servidor " + ex.Message); ;
+            }
+            finally
+            {
+                socket.Close();
+            }
+            Console.WriteLine("Precione uma tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SoketServer/SoketServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SoketServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar um objeto socket e definir um endereco de ip e porta para comunicacao
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+            socket.Bind(endPoint);
+            socket.Listen(5);//Escutar ate 5 vezes antes de parar
+
+            Console.WriteLine("Escutando...");
+
+            Socket escutar = socket.Accept();//Aceitar a requisisao
+
+            byte[] bytes = new byte[255];//Definir o tamanho da informacao recebida
+            int tamanho = escutar.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+
+            Array.Resize(ref bytes, tamanho);
+
+            Console.WriteLine("Cliente falou: ");
+            Console.WriteLine(Encoding.Default.GetString(bytes));
+
+            socket.Close();
+            Console.WriteLine("Servidor Finalizado\n Pressione qualquer tecla para finalizar");
+            Console.ReadKey();
+        }
+    }
+}
